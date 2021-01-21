f7adea045f7ca0dc56c69cf24cd3a84e448770be
Thriveni-123 <71956386+Thriveni-123@users.noreply.github.com>
2021-01-21 10:02:47 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    defaultServerResponse : {
+        status : 205,
+        message : '',
+        body : {}
+    },
+    requestValidationMessage: {
+        BAD_REQUEST: 'Invalid fields'
+    },
+    LOGINMESSAGE:{
+        ADDPROFILE:'Adding your image successfully',
+        PROFILE_FETCH:'Fetch your profile details suceessfully'
+    }
+}
